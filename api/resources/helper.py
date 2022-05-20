3e97eb25dfdf5dd0f3fb0364ab43cba7c1517a57
Chirayu Kapoor <dev.csociety@gmail.com>
2022-05-21 04:35:23 +0530

    Added error handling in file handling.

--- api/resources/helper.py
+++ api/resources/helper.py
@@ -2,9 +2,23 @@ import json
 
 class URLMapping:
     def get_url_mapping(self, filePath):
-        with open(filePath, "r") as url_mapping_file:
-            return json.load(url_mapping_file)
+        try:
+            with open(filePath, "r") as url_mapping_file:
+                try:
+                    data = json.load(url_mapping_file)
+                except Exception as e:
+                    print(f"While loading the json to Dict: \n{e}")
+                finally:
+                    data={}
+        except Exception as e:
+            print(f"File {filePath}: \n{e}")
+            data={}
+        finally:
+            return data
 
     def set_url_mapping(self, filePath, data):
-        with open(filePath, "w") as url_mapping_file:
-            json.dump(data, url_mapping_file)
+        try:
+            with open(filePath, "w") as url_mapping_file:
+                json.dump(data, url_mapping_file)
+        except Exception as e:
+            print(f"While unloading the json to Dict: \n {e}")
